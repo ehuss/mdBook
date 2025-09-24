dc7e56cff41bf038fb0f4176b283f3ffe16b2b27
Eric Huss <eric@huss.org>
2025-09-24 15:17:40 -0700

    Testing

---
+++ ci/update-dependencies.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Updates all compatible Cargo dependencies.
+
+set -ex
+
+cargo upgrade
+if git diff --quiet
+then
+    echo "No changes detected, exiting."
+    exit 0
+fi
+
+if git checkout update-dependencies
+then
+    git reset --hard origin/master
+else
+    git checkout -b update-dependencies
+fi
+
+git config user.name "github-actions[bot]"
+git config user.email "github-actions[bot]@users.noreply.github.com"
+
+git add Cargo.toml Cargo.lock
+git commit -m "Update cargo dependencies"
+
+git push origin update-dependencies
+
+gh pr create --title "Update cargo dependencies" \
+    --body "Automated update of Cargo dependencies" \
+    --head update-dependencies \
+    --base master
